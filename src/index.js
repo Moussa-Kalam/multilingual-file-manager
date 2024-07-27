d5e5756f2d603e407fa67919a479ac909109b41a
Moussa Kalam AMZAT <moussakalamamzat@gmail.com>
2024-07-27 11:40:16 +0200

    refactor: adjust global error handler

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-import express, { response } from 'express';
-import userRoutes from './routes/users.js';
-import fileRoutes from './routes/files.js';
+import express, { response } from "express";
+import userRoutes from "./routes/users.js";
+import fileRoutes from "./routes/files.js";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Welcome to our multilingual file manager platform!');
+app.get("/", (req, res) => {
+  res.send("Welcome to our multilingual file manager platform!");
 });
 
 app.use(express.json());
-app.use('/users', userRoutes);
-app.use('/files', fileRoutes);
+app.use("/users", userRoutes);
+app.use("/files", fileRoutes);
 
 app.use((error, request, response, next) => {
-  response.status(500).json({ status: 'error', message: error.message });
-})
+  const { message, status } = error;
+
+  response.status(status || 500).json({ message });
+});
 
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`);
 });
-
